269960793f6592e5ce452868842de3f736ee0708
Federico Cendali <federicocendali@gmail.com>
2023-04-17 22:35:30 -0300

    Se agrega lista de objetos estatica

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Button, TextInput } from 'react-native';
+import { styles } from './styles';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My new React Native App!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='Enter your event' style={styles.input} />
+        <Button title='Add' color='#52528C' />
+      </View>
+      <View style={styles.listContainer}>
+        <View style={styles.itemContainer}>
+          <Text style={styles.item}>Event 1</Text>
+        </View>
+        <View style={styles.itemContainer}>
+          <Text style={styles.item}>Event 2</Text>
+        </View>
+        <View style={styles.itemContainer}>
+          <Text style={styles.item}>Event 3</Text>
+        </View>
+        <View style={styles.itemContainer}>
+          <Text style={styles.item}>Event 4</Text>
+        </View>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FF8000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles.js
@@ -0,0 +1,39 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        marginHorizontal: 20,
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        marginTop: 55,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+    },
+    input: {
+        width: '80%',
+        borderBottomWidth: 1,
+        borderBottomColor: '#52528C',
+        color: '#212121',
+    },
+    listContainer: {
+        flex: 1,
+        backgoundColor: '#fff',
+        marginVertical: 20,
+    },
+    itemContainer: {
+        height: 80,
+        justifyContent: 'center',
+        backgroundColor: '#52528C',
+        borderRadius: 5,
+        marginVertical: 5,
+    },
+    item: {
+        marginHorizontal: 10,
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+});
