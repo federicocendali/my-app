c058a9ff6277ac2434954758315aed0eb076ed3d
Federico Cendali <federicocendali@gmail.com>
2023-04-25 01:56:39 -0300

    [feature] - add module pattern in architecture

---
+++ src/components/input/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { TextInput, Button, View } from 'react-native';
+import { styles } from "./styles";
+
+const Input = ({ placeholder, value, onChangeText, buttonColor, buttonTitle, onHandlerButton }) => {
+    return (
+        <View style={styles.inputContainer}>
+            <TextInput
+                placeholder={placeholder}
+                style={styles.input}
+                value={value}
+                onChangeText={(text) => onChangeText(text)}
+            />
+            <Button
+                title={buttonTitle}
+                color={buttonColor}
+                onPress={onHandlerButton}
+            />
+        </View>
+    )
+}
+
+export default Input;
---
+++ src/index.js
@@ -0,0 +1,90 @@
+import { useState } from 'react';
+import { Text, View, Button, FlatList, TouchableOpacity, Modal } from 'react-native';
+import { styles } from './styles';
+import { Input } from './components/index';
+
+export default function App() {
+    const [text, setText] = useState('');
+    const [events, setEvents] = useState([]);
+    const [modalVisible, setModalVisible] = useState(false);
+    const [selectedEvent, setSelectedEvent] = useState(null);
+
+    const onAddEvent = () => {
+        if (text.length === 0)
+            return;
+        setEvents([
+            ...events,
+            {
+                id: Math.random().toString(),
+                value: text
+            }
+        ]);
+        setText('');
+    };
+
+    const onHandlerEvent = (id) => {
+        setModalVisible(!modalVisible);
+        const selectedEvent = events.find(event => event.id === id);
+        setSelectedEvent(selectedEvent);
+    }
+
+    const onHandlerCancelModal = () => {
+        setModalVisible(!modalVisible);
+        setSelectedEvent(null);
+    }
+
+    const onHandlerDeleteEvent = (id) => {
+        setEvents(events.filter(event => event.id !== id));
+        setModalVisible(!modalVisible);
+    }
+
+    const renderItem = ({ item }) => {
+        return (
+            <TouchableOpacity style={styles.itemContainer} onPress={() => onHandlerEvent(item.id)}>
+                <Text style={styles.item}>{item.value}</Text>
+            </TouchableOpacity>
+        )
+    }
+
+    return (
+        <View style={styles.container}>
+            <Input
+                buttonColor='#52528C'
+                buttonTitle='Add'
+                onChangeText={(text) => setText(text)}
+                onHandlerButton={onAddEvent}
+                placeholder='Enter your event'
+                value={text}
+            />
+            <View style={styles.listContainer}>
+                <FlatList
+                    renderItem={renderItem}
+                    data={events}
+                    keyExtractor={(item) => item.id}
+                    alwaysBounceVertical={false}
+                />
+            </View>
+            <Modal visible={modalVisible} animationType='slide'>
+                <View style={styles.modalContainer}>
+                    <Text style={styles.modalTitle}>Event Detail</Text>
+                    <View style={styles.modalDetailContainer}>
+                        <Text style={styles.modalDetailMessage}>Are you sure to delete this item?</Text>
+                        <Text style={styles.selectedEvent}>{selectedEvent?.value}</Text>
+                    </View>
+                    <View style={styles.buttonContainer}>
+                        <Button
+                            title='Cancel'
+                            color='#5252BC'
+                            onPress={() => onHandlerCancelModal()}
+                        />
+                        <Button
+                            title='Delete'
+                            color='#5252BC'
+                            onPress={() => onHandlerDeleteEvent(selectedEvent.id)}
+                        />
+                    </View>
+                </View>
+            </Modal>
+        </View>
+    );
+}
